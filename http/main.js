405cdc34630cc6a572286429b7e369bd65d692af
nullfact0r <82207258+nullfact0r@users.noreply.github.com>
2021-05-28 23:41:02 +0200

    Fix JS

--- http/main.js
+++ http/main.js
@@ -1,5 +1,7 @@
 particlesJS.load("particles", "/particlesjs-config.json");
 
 fetch(`https://api.github.com/users/nullfact0r`)
-.then(res => res.json())
-.then(if(res.bio) res => document.getElementById("bio").innerHTML = res.bio);
+    .then((res) => res.json())
+    .then((res) => {
+        if (res.bio) document.getElementById("bio").innerHTML = res.bio;
+    });
